415294121a958bcb39eec9d7efced104587cad6c
cielavenir <cielartisan@gmail.com>
2013-01-14 05:09:28 +0900

    more codeeval

---
+++ tyama_codeeval42(TLE).rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+#TLE: submit "ruby tyama_codeeval42.rb tyama_codeeval42.in" result as PHP.
+def dfs(s,d,l)
+	if d==l
+		r=eval(s.tr(' ','').gsub(/^0([1-9]+)/,'\1').gsub(/([+-])0([1-9]+)/,'\1\2')).abs
+		[2,3,5,7].any?{|e|r%e==0}?1:0
+	else
+		r=0
+		'+- '.each_byte{|e|
+			s[d*2+1,1]=e.chr
+			r+=dfs(s,d+1,l)
+		}
+		r
+	end
+end
+while gets
+	s=$_.chomp
+	l=s.size-1
+	s=s.split('').join(' ')
+	p dfs(s,0,l)
+end
---
+++ tyama_codeeval60.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+D=[[-1,0],[1,0],[0,-1],[0,1]]
+$h={[0,0]=>1}
+#def s(n) n.abs.to_s.split('').map(&:to_i).reduce(:+) end
+def s(n)
+	r=0
+	x=n.abs
+	while x>0
+		r+=x%10
+		x/=10
+	end
+	r
+end
+queue=[[0,0]]
+until queue.empty?
+	x,y=queue.shift
+	D.each{|e|
+		if !$h[[x+e[0],y+e[1]]] && s(x+e[0])+s(y+e[1])<20
+			$h[[x+e[0],y+e[1]]]=1
+			queue<<[x+e[0],y+e[1]]
+		end
+	}
+end
+p $h.size
