d419f0135c2b2083c1f9933dcb055189dd4e7787
cielavenir <cielartisan@gmail.com>
2013-01-22 19:29:43 +0900

    another try

--- tyama_rosalindGCON(WA).rb
+++ tyama_rosalindGCON(WA).rb
@@ -11,12 +11,12 @@ end
 def alignment(x, y)
 	#initialize
 	a = Array.new(x.length+1){Array.new(y.length+1, 0)}
-	back = Array.new(x.length+1){Array.new(y.length+1, 0)}
+	back = Array.new(x.length+1){Array.new(y.length+1)};back[0][0]=[0,0,'']
 	tx = ""; ty = ""; t=""
 
 	#DP
-	1.step(a.length-1){|i| a[i][0] = a[i-1][0] - 5;back[i][0]=[i-1,0,  "a"]}
-	1.step(a[0].length-1){|j| a[0][j] = a[0][j-1] - 5;back[0][j]=[0,  j-1,"b"]}
+	1.step(a.length-1){|i| a[i][0] = a[i-1][0] - (back[i-1][0][2]=="a"?0:5);back[i][0]=[i-1,0,  "a"]}
+	1.step(a[0].length-1){|j| a[0][j] = a[0][j-1] - (back[0][j-1][2]=="b"?0:5);back[0][j]=[0,  j-1,"b"]}
 	1.step(a.length-1){|i|
 		1.step(a[0].length-1){|j|
 			z = maximum3([a[i-1][j-1]+$blosum62[x[i-1].chr][y[j-1].chr], a[i-1][j]-(back[i-1][j][2]=="a"?0:5), a[i][j-1]-(back[i][j-1][2]=="b"?0:5)])
