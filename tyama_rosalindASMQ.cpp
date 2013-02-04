375005d123de0047d63fde63143a76b07fc91183
cielavenir <cielartisan@gmail.com>
2013-02-04 15:03:02 +0900

    more rosalind, codeiq186 opened

---
+++ tyama_rosalindASMQ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+main(){
+	string s;
+	vector<int>v;
+	int i;
+	for(;cin>>s;)for(i=0;i<s.length();i++)v.push_back(s.length());
+	sort(v.begin(),v.end());
+	cout<<((v.size()&1)?v[v.size()/2]:(v[v.size()/2]+v[v.size()/2+1])/2)<<' '<<((v.size()&2)?v[v.size()/4]:(v[v.size()/4]+v[v.size()/4+1])/2)<<endl;
+}
---
+++ tyama_rosalindGASM.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+class String
+	def complement
+		self.reverse.tr("ATGC","TACG")
+	end
+end
+a=[]
+while gets
+a<<$_.chomp
+end
+b=a.map{|e|e.complement}
+l=a[0].length
+(l-1).downto(1){|k|
+	h1={}
+	h2={}
+	#de Brujin graph
+	a.each{|s|(l-k).times{|i|h1[s[i...(i+k)]]=s[(i+1)...(i+1+k)]}}
+	b.each{|s|(l-k).times{|i|h2[s[i...(i+k)]]=s[(i+1)...(i+1+k)]}}
+	f=x=h1.keys[0]
+	s=''
+	while h1[x]||h2[x]
+		s+=x[-1,1]
+		x = h1[x] ? h1.delete(x) : h2.delete(x)
+		if x==f
+			puts s
+			exit
+		end
+	end
+}
+raise 'some of reads are corrupted'
