ce82b660587ef08b18417d264202b8cdfe5cf621
cielavenir <cielartisan@gmail.com>
2013-02-04 15:07:25 +0900

    fixed bug

--- tyama_rosalindASMQ.cpp
+++ tyama_rosalindASMQ.cpp
@@ -9,5 +9,5 @@ main(){
 	int i;
 	for(;cin>>s;)for(i=0;i<s.length();i++)v.push_back(s.length());
 	sort(v.begin(),v.end());
-	cout<<((v.size()&1)?v[v.size()/2]:(v[v.size()/2]+v[v.size()/2+1])/2)<<' '<<((v.size()&2)?v[v.size()/4]:(v[v.size()/4]+v[v.size()/4+1])/2)<<endl;
+	cout<<((v.size()&1)?(v[v.size()/2]+v[v.size()/2+1])/2:v[v.size()/2])<<' '<<((v.size()&2)?(v[v.size()/4]+v[v.size()/4+1])/2:v[v.size()/4])<<endl;
 }
