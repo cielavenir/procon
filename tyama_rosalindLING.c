4e384b662fcbcf73d3155131adcdd3a6930b21a2
cielavenir <cielartisan@gmail.com>
2013-02-06 23:59:39 +0900

    fixed LING properly

--- tyama_rosalindLING.c
+++ tyama_rosalindLING.c
@@ -2,27 +2,24 @@
 #include <stdio.h>
 #define lst_string_fromstr(s) lst_string_new(s,1,strlen(s))
 char k[99999];
-double N;
-//int i;
+long long N;
 int z(LST_Node *node, void *data){
 	if(lst_node_is_root(node))return 1;
 	int x=lst_edge_get_length(node->up_edge);
-	N+=x-0.5; //something is completely broken...
-	//fprintf(stderr,"%d\n",++i);
+	N+=x-lst_node_is_leaf(node); //what is this "1"? maybe terminal mark of leaf?
 	return 1;
 }
 main(i,mul){
 	long long n,d,p,j,x;
 	scanf("%s",k);
 	n=strlen(k);
-	d=0;p=1;mul=4;
+	d=0;p=1;
 	for(i=1;i<=n;i++){
 		j=n+1-i;
-		p*=mul;
+		p*=4;
 		if(p<j){
 			d+=p;
 		}else{
-			//mul=1;
 			d+=j*(j+1)/2;
 			break;
 		}
@@ -32,7 +29,6 @@ main(i,mul){
 	lst_stree_init(&tree);
 	lst_stree_add_string(&tree,lst_string_fromstr(k));
 	lst_alg_dfs(&tree,z,NULL);
-	//printf("%.0f\n",N);
-	printf("%.4f\n",N/d); //N should be the sum of the length of all edges...?
-	//It can still give near data for larger data though.
+	//printf("%lld\n",N);
+	printf("%f\n",N*1.0/d); //N == sum of the length of all edges
 }
