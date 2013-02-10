86c541c18f514938e0b7c670fdc04eba451f2e0f
cielavenir <cielartisan@gmail.com>
2013-02-10 22:17:49 +0900

    clean

--- tyama_aizu2438(WA).rb
+++ tyama_aizu2438(WA).rb
@@ -3,7 +3,7 @@
 require 'yaml'
 a=gets.chomp[1..-1].split('.')
 h=YAML.load(STDIN.read.gsub(/([a-z0-9][a-z0-9 ]*)/,'!str \1'))
-p h
+#p h
 a.each{|e|
 	if h.class.name=='String' then puts 'no such property';exit end
 	h=h[e]
