788102fa8dcbfd893b65562bd74eb9ed9bd970c7
cielavenir <cielartisan@gmail.com>
2013-02-13 17:56:44 +0900

    icpc2012dA Haskell

--- tyama_codeiq186.py
+++ tyama_codeiq186.py
@@ -6,13 +6,18 @@ from nltk.corpus import gutenberg
 #words2 = [w for w in words1 if re.sub(r'[^a-z]','',w)==w]
 words2 = [w.lower() for w in gutenberg.words('austen-sense.txt') if w.isalpha()] ###
 freq=nltk.FreqDist(words2)
-keys = freq.values() ###
+s = len(words2)
+for e in freq.keys():
+  print "%s,%d,%f" % (e,freq[e],float(freq[e])/s*100)
+
+'''
+keys = freq.keys() ###
 values = freq.values() ###
 #s=sum(freq.values())
 s=sum(values) ### len(words2)
 for i in range(len(freq)):
   #print "%s,%d,%f" % (freq.keys()[i],freq.values()[i],float(freq.values()[i])/s*100)
   print "%s,%d,%f" % (keys[i],values[i],float(values[i])/s*100) ###
-
+'''
 #冠詞、前置詞、代名詞、接続詞が多い
 #主人公であるelinorとmarianneも多く出現する
--- tyama_icpc2007dA.hs
+++ tyama_icpc2007dA.hs
@@ -1,34 +1,19 @@
 #!/usr/bin/runhugs
-module Main where
-
--- http://d.hatena.ne.jp/g940425/20110804/1312434821
-getIntLine :: IO Int
-getIntLine = do
-	n <- getLine
-	return(read n::Int)
-putAnsLn :: (Show a) => a -> IO ()
-putAnsLn ans = putStrLn(show ans)
-getIntLines :: Int -> IO [Int]
-getIntLines n =
-	if n>0
-		then do
-			l <- getIntLine
-			ls <- getIntLines (n-1)
-			return (l:ls)
-		else
-			return []
-
-mysum :: [Int] -> Int -> Int -> Int -> Int -> Int -> Int
-mysum a n ma mi s z =
+-- mysum :: Int -> Int -> Int -> Int -> Int -> IO Int
+mysum n ma mi s z = do
 	if n==0
-		then floor(fromIntegral(s-ma-mi)/fromIntegral(z-2))
-		else mysum a (n-1) (max ma (a !! (n-1))) (min mi (a !! (n-1))) (s+(a !! (n-1))) z
+		then
+			return (div (s-ma-mi) (z-2)) --(floor(fromIntegral(s-ma-mi)/fromIntegral(z-2)))
+		else do
+			x <- readLn
+			y <- mysum (n-1) (max ma x) (min mi x) (s+x) z
+			return (y)
 
 main = do
-	n<-getIntLine
+	n <- readLn
 	if n/=0
 		then do
-			a<-getIntLines n -- need to use list? lol
-			putAnsLn(mysum a n 0 1000 0 n);
+			s <- mysum n 0 1000 0 n
+			putStrLn (show s)
 			main
 		else putStr "" --dummy
