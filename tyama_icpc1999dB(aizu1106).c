3e81cf545b8f615023af8582dfe2c0cbf585d3b2
cielavenir <cielartisan@gmail.com>
2013-03-13 20:17:54 +0900

    optimized icpc1999dB

--- tyama_icpc1999dB(aizu1106).c
+++ tyama_icpc1999dB(aizu1106).c
@@ -1,16 +1,7 @@
-#include <assert.h>
-main(a,b,c,p,q,r,s,i){for(;scanf("%d%d%d",&a,&b,&c),a;){
-	if(!c){
-		if(a==1&&b<0)printf("1 0 %d %d\n",a,b);
-		else printf("%d %d 1 0\n",a,b);
-		continue;
-	}
-	for(r=1;r*r<=a;r++)if(a%r==0)
-		for(p=a/r,s=1;s<=abs(c);s++)if(c%s==0)
-			for(i=0;i<2;i++,s=-s)if(q=c/s,p*s+q*r==b){
-				if(p==r&&q<s)printf("%d %d %d %d\n",r,s,p,q);
-				else printf("%d %d %d %d\n",p,q,r,s);
-				goto next;
-			}
+main(a,b,c,p,q,r,s,i,j){for(;scanf("%d%d%d",&a,&b,&c),a;){
+if(!c)a==1&&b<0?printf("1 0 %d %d\n",a,b):printf("%d %d 1 0\n",a,b);
+else{
+	for(r=1;r*r<=a;r++)if(a%r==0)for(p=a/r,s=1;s*s<=abs(c);s++)if(c%s==0)for(j=0;j<2;j++,s=c/s)for(i=0;i<2;i++,s=-s)if(q=c/s,p*s+q*r==b)
+		{p==r&&q<s?printf("%d %d %d %d\n",r,s,p,q):printf("%d %d %d %d\n",p,q,r,s);goto next;}
 	puts("Impossible");
-next:;}exit(0);}
+}next:;}exit(0);}
