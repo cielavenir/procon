cfc2d276616f91fc7098c1a1f66df8ecc0b6882c
cielavenir <cielartisan@gmail.com>
2013-03-19 02:05:36 +0900

    keep github streak

---
+++ tyama_8946take32.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require 'mechanize'
+mech=Mechanize.new
+#prepare...
+uri=URI.parse('http://www.hackerschool.jp/hack/take32.php')
+str='PHPSESSID='
+Mechanize::Cookie.parse(uri, str){|c|mech.cookie_jar.add(uri, c)}
+#execute!
+mech.get(uri)
+form=mech.page.forms[4]#_with(:action=>'./take32.php')
+form['input_id']=mech.page.at('div#question').text.split(' x ').map(&:to_i).reduce(:*)
+#form.add_field!('time_set',1)
+form.submit
+#puts mech.page.body
