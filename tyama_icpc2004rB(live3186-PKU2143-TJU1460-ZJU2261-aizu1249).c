9a7b7d7f3f5414833340d3bad9b3f39c66a49d37
cielavenir <cielartisan@gmail.com>
2013-03-24 20:24:42 +0900

    optimized icpc2004rB

--- tyama_icpc2004rB(live3186-PKU2143-TJU1460-ZJU2261-aizu1249).c
+++ tyama_icpc2004rB(live3186-PKU2143-TJU1460-ZJU2261-aizu1249).c
@@ -1,15 +1,13 @@
-#define X(V,N)0<=V+D[d][N]*(m-1)&&V+D[d][N]*(m-1)<n
 D[13][3]={{1,0,0},{0,1,0},{0,0,1},{1,1,0},{1,0,1},{0,1,1},{1,-1,0},{1,0,-1},{0,1,-1},{1,1,1},{-1,1,1},{1,-1,1},{1,1,-1}};
-M[7][7][7];main(n,m,p,I,x,y,i,j,k,c,d,z){for(;scanf("%d%d%d",&n,&m,&p),n;){
-	for(memset(M,0,sizeof(M)),c=1,I=0;I<p;c*=-1){
-		scanf("%d%d",&x,&y),x--,y--,I++;
-		for(i=0;;i++)if(!M[x][y][i]){M[x][y][i]=c;break;}
-		for(i=0;i<n;i++)for(j=0;j<n;j++)for(k=0;k<n;k++)for(d=0;d<13;d++)if(X(i,0)&&X(j,1)&&X(k,2)){
-			for(z=0;z<m;z++)if(M[i+D[d][0]*z][j+D[d][1]*z][k+D[d][2]*z]!=c)break;
-			if(z==m){printf("%s %d\n",c==1?"Black":"White",I);goto next;}
+M[9][9][9],q[2];main(n,m,p,I,x,y,z,j,c,d){for(;scanf("%d%d%d",&n,&m,&p),n;){
+	for(memset(M,I=0,sizeof(M)),c=1;I++<p;c*=-1){
+		for(scanf("%d%d",&x,&y),z=1;;z++)if(!M[x][y][z]){M[x][y][z]=c;break;}
+		for(d=0;d<13;d++){
+			for(j=0;j<2;j++)for(q[j]=0;M[x+D[d][0]*q[j]][y+D[d][1]*q[j]][z+D[d][2]*q[j]]==c;q[j]+=2*j-1);
+			if(q[1]-q[0]-1>=m){printf(c==1?"Black %d\n":"White %d\n",I);goto next;}
 		}
 	}
 	puts("Draw");
 next:
-	for(;I<p;I++)scanf("%d%d",&x,&y);
+	for(;I++<p;)scanf("%d%d",&x,&y);
 }exit(0);}
