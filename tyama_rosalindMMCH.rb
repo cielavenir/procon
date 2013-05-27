52972276f9d17034108ea2294a03476f7c4ddb8a
cielavenir <cielartisan@gmail.com>
2013-05-27 22:04:54 +0900

    fixed broken ruby sources

--- tyama_rosalindMMCH.rb
+++ tyama_rosalindMMCH.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
 s=$<.drop(1).map(&:chomp)*''
-au=[s.count 'A',s.count 'U'].sort
-cg=[s.count 'C',s.count 'G'].sort
+au=[s.count('A'),s.count('U')].sort
+cg=[s.count('C'),s.count('G')].sort
 p (au[1]-au[0]+1..au[1]).reduce(:*)*(cg[1]-cg[0]+1..cg[1]).reduce(:*)
