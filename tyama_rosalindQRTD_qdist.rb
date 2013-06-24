d5f2eb19ea751ddb530b0265a049f3f51c96b5bf
cielavenir <cielartisan@gmail.com>
2013-06-25 03:10:55 +0900

    fix var

--- tyama_rosalindQRTD_qdist.rb
+++ tyama_rosalindQRTD_qdist.rb
@@ -2,14 +2,14 @@
 #prepare http://birc.au.dk/software/qdist/
 require 'tempfile'
 QDIST='qdist'
-Tempfile.open('fasta1','.'){|fasta1|
-Tempfile.open('fasta2','.'){|fasta2|
+Tempfile.open('tree1','.'){|tree1|
+Tempfile.open('tree2','.'){|tree2|
 	gets
-	fasta1.puts gets
-	fasta1.flush
-	fasta2.puts gets
-	fasta2.flush
-	io=open(%Q(| "#{QDIST}" #{File.basename fasta1.path} #{File.basename fasta2.path}))
+	tree1.puts gets
+	tree1.flush
+	tree2.puts gets
+	tree2.flush
+	io=open(%Q(| "#{QDIST}" #{File.basename tree1.path} #{File.basename tree2.path}))
 	io.gets
 	q=io.gets.split[6].to_i
 	puts q*2
