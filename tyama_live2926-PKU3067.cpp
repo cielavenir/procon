0342cce981aa35d79b92a9a8e2321b30b5fa13b7
cielavenir <cielartisan@gmail.com>
2013-07-09 18:23:27 +0900

    more PKU

---
+++ tyama_live2926-PKU3067.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int A[999999], W[999999];
+long long merge_and_count(int l, int r) { // range [l,r)
+	if (l+1 >= r) return 0; // empty
+	if (l+2 == r) { // [l,r) == [l,l+1] 要素2つだけ
+		if (A[l] <= A[l+1]) return 0; // 逆転はなし
+		swap(A[l], A[l+1]);
+		return 1; // 逆転一つ
+	}
+	int m = (l+r)/2; // [l,r) == [l,m) + [m,r)
+	long long cl = merge_and_count(l, m); // 左半分を再帰的に数える
+	long long cr = merge_and_count(m, r); // 右半分を再帰的に数える
+	long long c = 0; // 左と右を混ぜるときの逆点数
+	int i=l, j=m; // iは[l,m)を動き, jは[m,r)を動いて、
+	int k=l; // 小さいものから W[k] に書き込む
+	while (i<m && j<r) { // A[i]とA[j]を比べながら進む
+		if (A[i] <= A[j]) W[k++] = A[i++]; // 左半分の方が小さく逆転なし
+		else {
+			W[k++] = A[j++];
+			c += m-i; // 左半分の方が大きい、左半分で未処理の要素だけ飛び越える
+		}
+	}
+	while (i<m) W[k++] = A[i++]; // 左半分が余った場合
+	while (j<r) W[k++] = A[j++]; // 右半分が余った場合
+	//assert(k == r);
+	copy(W+l, W+r, A+l);
+	return cl + cr + c;
+}
+main(){
+	int t=0,T,N,i,x,y;
+	for(scanf("%d",&T);t<T;){
+		vector<pair<int,int> >v;
+		for(scanf("%d%d%d",&N,&N,&N),i=0;i<N;i++)scanf("%d %d",&x,&y),v.push_back(make_pair(x,y));
+		sort(v.begin(),v.end());
+		for(i=0;i<N;i++)A[i]=v[i].second;
+		printf("Test case %d: %lld\n",++t,merge_and_count(0,N));
+	}
+}
