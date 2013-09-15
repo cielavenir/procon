aba30faac2160d59c747a65405f7e0fc712ce0ab
cielavenir <cielartisan@gmail.com>
2013-09-15 19:14:25 +0900

    aizu 2529

---
+++ tyama_aizu2529.c
@@ -0,0 +1,33 @@
+Z=8,ptr[20],c,d,e,s,i,j;
+main(){
+#if 1
+	for(i=0;i*Z<128;i++)ptr[i]=i*Z;
+	for(i=0;i<Z;i++)putchar('+');
+	putchar('[');
+	for(i=0;i*Z<128;i++)for(putchar('>'),j=0;j<i;j++)putchar('+');
+	for(i=0;i*Z<128;i++)putchar('<');
+	putchar('-'),putchar(']');
+	for(e=-1;~(c=getchar())&&c!='\n';putchar('.')){
+		if(s='>',d=c/Z-e,d<0)d=-d,s='<';
+		for(i=0;i<d;i++)putchar(s);
+		e=c/Z;
+		if(s='+',d=c-ptr[c/Z],d<0)d=-d,s='-';
+		for(i=0;i<d;i++)putchar(s);
+		ptr[c/Z]=c;
+	}
+#else
+	putchar('>');
+	for(e=0;~(c=getchar())&&c!='\n';putchar('.')){
+		if(s='+',d=c-e,d<0)d=-d,s='-';
+		if(d/Z){
+			putchar('<');
+			for(i=0;i<d/Z;i++)putchar('+');
+			putchar('['),putchar('>');
+			for(i=0;i<Z;i++)putchar(s);
+			putchar('<'),putchar('-'),putchar(']'),putchar('>');
+		}
+		for(i=0;i<d%Z;i++)putchar(s);
+		e=c;
+	}
+#endif
+exit(0);}
