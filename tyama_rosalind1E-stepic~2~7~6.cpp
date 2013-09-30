474476f9d4999e9882aac5edcae34f16ab5777fc
cielavenir <cielartisan@gmail.com>
2013-09-30 20:04:54 +0900

    fixed rosalind 1E

--- tyama_rosalind1E-stepic~2~7~6.cpp
+++ tyama_rosalind1E-stepic~2~7~6.cpp
@@ -10,13 +10,13 @@ int main(){
 	for(cin>>s;i<s.size();i++){
 		if(s[i]=='C'){
 			if(m>--k)m=k,m_idx.clear();
-			if(m==k)m_idx.push_back(i);
 		}
 		if(s[i]=='G')k++;
+		if(m==k)m_idx.push_back(i);
 	}
 	for(i=0;i<m_idx.size();i++){
 		if(i)cout<<' ';
-		cout<<(m_idx[i]+1); //waiting for stepic team's answer
+		cout<<(m_idx[i]+1);
 	}
 	cout<<endl;
 }
