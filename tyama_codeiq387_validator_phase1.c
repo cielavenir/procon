6d4e61b964aebba250104317f3f284085ef73343
cielavenir <cielartisan@gmail.com>
2013-10-01 17:08:31 +0900

    revisited codeiq 387

---
+++ tyama_codeiq387_validator_phase1.c
@@ -0,0 +1,103 @@
+//compatible with C++ mode
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define N 9
+char s[N][N*2];
+int Z[10],V[N][N];
+int check(){
+	int i=1;
+	for(;i<=9;i++)if(Z[i]!=1)return 0;
+	return 1;
+}
+int C=0,M=0;
+int main(){for(;;){
+	int r1=0,r2=0,r3=0,I,J,i,j;
+	for(I=0;I<N;I++){
+		if(scanf("%s",s[I])==EOF)return 0;
+		assert(strlen(s[I])==N);
+		for(i=0;i<N;i++){
+			assert('1'<=s[I][i]&&s[I][i]<='9');
+			V[I][i]=s[I][i]-'0';
+		}
+	}
+	for(I=0;I<N-8;I++)for(J=0;J<N;J++){
+		memset(Z,0,sizeof(Z));
+		for(i=0;i<9;i++)Z[V[I+i][J]]++;
+		r1+=check();
+	}
+	for(I=0;I<N;I++)for(J=0;J<N-8;J++){
+		memset(Z,0,sizeof(Z));
+		for(j=0;j<9;j++)Z[V[I][J+j]]++;
+		r2+=check();
+	}
+	for(I=0;I<N-2;I++)for(J=0;J<N-2;J++){
+		memset(Z,0,sizeof(Z));
+		for(i=0;i<3;i++)for(j=0;j<3;j++)Z[V[I+i][J+j]]++;
+		r3+=check();
+	}
+	//printf("Case %d:\n",++C);
+	//printf("vertical   score : %6d / %d\n",r1,N*(N-8));
+	//printf("horizontal score : %6d / %d\n",r2,N*(N-8));
+	//printf("3x3 block  score : %6d / %d\n",r3,(N-2)*(N-2));
+	//printf("total      score : %6d / %d\n",r1+r2+r3,2*N*(N-8)+(N-2)*(N-2));
+	++C;
+	if(M<r1+r2+r3){
+		M=r1+r2+r3;
+		printf("Found %d solution (case %d):\n",M,C);
+		for(I=0;I<N;I++)puts(s[I]);
+		fflush(stdout);
+	}
+}}
+/*
+Found 43 solution (case 1):
+123456789
+456789123
+789123456
+214365897
+365897214
+897214365
+531642978
+642978531
+978531642
+Found 44 solution (case 88593):
+123456789
+456789123
+789123456
+214365897
+395817264
+867294315
+542631978
+931578642
+678942531
+Found 45 solution (case 37482865):
+123456789
+456789123
+789123456
+214938675
+635274918
+978615234
+341892567
+562347891
+897561342
+Found 46 solution (case 54522949):
+123456789
+456789123
+789123456
+215397864
+364815297
+897264315
+631578942
+542931678
+978642531
+Found 51 solution (case 178882633):
+123456789
+456789123
+789123456
+231564897
+564897231
+897231564
+312645978
+645978312
+978312645
+*/
