5d8872dc755d75057f532613f8519a75c8c12b86
cielavenir <cielartisan@gmail.com>
2013-10-02 21:34:29 +0900

    rosalind INI6 Python3 compat

--- tyama_rosalindINI6-stepic5~32~6.py
+++ tyama_rosalindINI6-stepic5~32~6.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
+from __future__ import print_function
 from collections import Counter
-for k,v in Counter(raw_input().split()).items(): print k,v
+import sys
+if sys.version_info[0]>=3: raw_input=input
+for k,v in Counter(raw_input().split()).items(): print(k,v)
