ec024af9971b6f1422817c40b1c4be7c663b7048
cielavenir <cielartisan@gmail.com>
2013-12-04 19:23:23 +0900

    main HER

---
+++ tyama_mainHER.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int n,i,h,m;
+	string s;
+	cin>>n>>s;
+	h=count(s.begin(),s.end()-1,'H');
+	m=count(s.begin(),s.end()-1,'M');
+	cout<<(n<2?"HM":h>m||(h==m&&s[n-2]-'H')?"H":"M")<<endl;
+}
