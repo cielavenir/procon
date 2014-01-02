10c19d60b7f247678610b04319935880a789186a
cielavenir <cielartisan@gmail.com>
2014-01-02 19:10:26 +0900

    optimized hena17pre

--- tyama_hena17pre.cpp
+++ tyama_hena17pre.cpp
@@ -1,24 +1,23 @@
 //http://qiita.com/Nabetani/items/dabe8ec57e0313229552
 //http://nabetani.sakura.ne.jp/hena/ord17scheherazade/
 #include <vector>
-#include <algorithm>
 #include <cstdio>
 using namespace std;
 
 int main(){
-	int n,n0,i;
+	int n,n0,i,j;
 	for(;~scanf("%d",&n);){
 		if(n<3)puts("-");
 		else{
 			vector<int>r;
-			for(i=2;i<n;i++){
+			for(i=2;i<n/2;i++){
 				n0=n;
 				vector<int>a;
 				for(;n0;n0/=i)a.push_back(n0%i);
-				vector<int>b=a;
-				reverse(a.begin(),a.end());
-				if(a==b)r.push_back(i);
+				for(j=0;j<a.size()/2;j++)if(a[j]!=a[a.size()-j-1])break;
+				if(j==a.size()/2)r.push_back(i);
 			}
+			r.push_back(n-1);
 			for(i=0;i<r.size();i++)printf(i<r.size()-1?"%d,":"%d\n",r[i]);
 		}
 		fflush(stdout);
--- tyama_hena17pre.rb
+++ tyama_hena17pre.rb
@@ -4,7 +4,7 @@
 STDOUT.sync=true
 while gets
 	n=$_.to_i
-	puts n<3 ? '-' : 2.step(n-1).select{|i|
+	puts n<3 ? '-' : (2.step((n-1)/2).select{|i|
 		n0=n
 		a=[]
 		while n0>0
@@ -12,5 +12,5 @@ while gets
 			n0/=i
 		end
 		a==a.reverse
-	}*','
+	}+[n-1])*','
 end
