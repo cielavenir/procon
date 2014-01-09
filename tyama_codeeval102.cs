b757a036a6a0ecd9c0d6d18fb941fd96d0de6889
cielavenir <cielartisan@gmail.com>
2014-01-09 22:46:24 +0900

    codeeval 102 Go

---
+++ tyama_codeeval102.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+class CodeEval102{
+	static void Main(string[] args){
+		string str;
+		for(;(str=Console.ReadLine())!=null;){
+			var serializer = new DataContractJsonSerializer(typeof(Data));
+			Data data = (Data)serializer.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(str)));
+			int ret=0;
+			foreach(Data.Item item in data.menu.items){
+				if(item!=null && item.label!=null)ret+=item.id;
+			}
+			Console.WriteLine(ret);
+		}
+	}
+}
+
+[DataContract]
+public class Data
+{
+	[DataContract]
+	public class Item
+	{
+		[DataMember]
+		public int id { get; set; }
+		[DataMember]
+		public string label { get; set; }
+	}
+	[DataContract]
+	public class Menu
+	{
+		[DataMember]
+		public string header { get; set; }
+		[DataMember]
+		public List<Item> items { get; set; }
+	}
+	[DataMember]
+	public Menu menu { get; set; }
+}
---
+++ tyama_codeeval102.go
@@ -0,0 +1,35 @@
+package main
+import(
+	"os"
+	"bufio"
+	"encoding/json"
+	"fmt"
+)
+type Item struct{
+	Id int
+	Label *string
+}
+type Menu struct{
+	Header *string
+	Items []*Item
+}
+type Data struct{
+	Menu Menu
+}
+
+func main(){
+	reader:=bufio.NewReader(os.Stdin)
+	for {
+		s,err := reader.ReadString('\n')
+		if err!=nil { break }
+		var data Data
+		err = json.Unmarshal([]byte(s),&data)
+		r:=0
+		if err==nil {
+			for _,item := range data.Menu.Items {
+				if item!=nil && item.Label!=nil { r+=item.Id }
+			}
+		}
+		fmt.Println(r)
+	}
+}
