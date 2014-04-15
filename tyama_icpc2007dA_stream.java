91638ca485b48189ec69aaae113102d5a8c6eb01
cielavenir <cielartisan@gmail.com>
2014-04-15 13:47:32 +0900

    icpc2007dA Java Stream

---
+++ tyama_icpc2007dA_stream.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+class Main{
+	public static void main(String[]z){
+		Scanner c=new Scanner(System.in);
+		for(int n;(n=c.nextInt())>0;)System.out.println((int)IntStream.range(0,n).map(e->c.nextInt()).sorted().skip(1).limit(n-2).average().getAsDouble());
+	}
+}
