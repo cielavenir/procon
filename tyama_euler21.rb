46c74a858d7bcee0da8defed96b392cf52fe28d3
cielavenir <cielartisan@gmail.com>
2014-07-31 19:12:40 +0900

    submit icpc2014dp to aizu

--- tyama_euler21.rb
+++ tyama_euler21.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/ruby
 require 'prime'
 h={}
-2.step(9999){|i|h[i]=i.prime_division.reduce(1){|s,e|s*(e[0]**(e[1]+1)-1)/(e[0]-1)}-i}
-p 2.step(9999).select{|i|x=h[i];x!=i&&h[x]==i}.reduce(:+)
+#2.step(1000000){|i|h[i]=i.prime_division.reduce(1){|s,e|s*(e[0]**(e[1]+1)-1)/(e[0]-1)}-i}
+#a=2.step(1000000).select{|i|x=h[i];x!=i&&h[x]==i}
+a=[220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368, 10744, 10856, 12285, 14595, 17296, 18416, 63020, 66928, 66992, 67095, 69615, 71145, 76084, 79750, 87633, 88730]
+gets.to_i.times{
+	n=gets.to_i
+	p a.select{|e|e<n}.reduce(:+)
+}
+__END__
+1
+10000
