aeb2dc92da8b4eb437c46cc0b2ecc10dc6909f96
cielavenir <cielartisan@gmail.com>
2014-08-22 01:22:24 +0900

    update euler35

--- euler/tyama_euler35.rb
+++ euler/tyama_euler35.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 require 'prime';
 h={};a=[]
-Prime.each(999999){|e|h[e]=1;a<<e.to_s}
-p a.count{|s|s.size.times.map{|i|h[(s[i,s.size-i]+s[0,i]).to_i]}.all?}
+Prime.each(gets.to_i-1){|e|h[e]=1;a<<e.to_s}
+p a.select{|s|s.size.times.map{|i|h[(s[i,s.size-i]+s[0,i]).to_i]}.all?}.map(&:to_i).reduce(:+)
+#{}.size for euler
+__END__
+1000000
