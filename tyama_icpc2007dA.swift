66bef1335c191e0447e983e53c4d0fc8e484f164
cielavenir <cielartisan@gmail.com>
2014-09-10 17:44:21 +0900

    trying to fix icpc2007dA swift

--- tyama_icpc2007dA.swift
+++ tyama_icpc2007dA.swift
@@ -1,3 +1,4 @@
+import Darwin
 var n=0
 scanf("%d".UTF8String(),&n)
 while n>0 {
@@ -5,13 +6,12 @@ while n>0 {
 	var mi=1000
 	var sum=0
 	var s=0
-	var sum=0
-	for i in 0..n {
+	for i in 0..<n {
 		scanf("%d".UTF8String(),&n)
 		if ma<s {ma=s}
 		if mi>s {mi=s}
 		sum+=s
 	}
-	printf("%d\n".UTF8String(), (sum-ma-mi)/(n-2))
+	println((sum-ma-mi)/(n-2))
 	scanf("%d".UTF8String(),&n)
 }
