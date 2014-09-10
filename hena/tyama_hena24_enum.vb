8f8738e1432fc651bd2d719d26cf01032154d9a7
cielavenir <cielartisan@gmail.com>
2014-09-10 17:45:26 +0900

    hena24 VB

--- hena/tyama_hena24_enum.vb
+++ hena/tyama_hena24_enum.vb
@@ -8,24 +8,24 @@ imports System.Runtime.InteropServices
 
 module Hena24
 	<DllImport("c")>
-	private shared function cbrt(ByVal d as double) as double
+	private function cbrt(ByVal d as double) as double
 	end function
 
 	private function is_sq(ByVal n as integer) as boolean
 		dim x as integer=cint(Math.Sqrt(n))
-		is_sq = x*x=n
+		return x*x=n
 	end function
 
 	private function is_cb(ByVal n as integer) as boolean
 		dim x as integer=cint(cbrt(n))
-		is_cb = x*x*x=n
+		return x*x*x=n
 	end function
 
 	private function is_multiple(ByVal i as integer,ByVal n as integer) as boolean
-		is_multiple = (i mod n=0)
+		return i mod n=0
 	end function
 	private function is_le(ByVal i as integer,ByVal n as integer) as boolean
-		is_le = i<=n
+		return i<=n
 	end function
 
 	iterator function generate() as IEnumerable(of integer)
@@ -36,7 +36,7 @@ module Hena24
 		end while
 	end function
 
-	iterator function drop_prev(ByRef check as Func(of integer,boolean),ByRef _prev as IEnumerable(of integer)) as IEnumerable(of integer)
+	iterator function drop_prev(ByVal check as Func(of integer,boolean),ByVal _prev as IEnumerable(of integer)) as IEnumerable(of integer)
 		dim prev as IEnumerator(of integer)=_prev.GetEnumerator()
 		prev.MoveNext()
 		dim a as integer=prev.Current
@@ -52,7 +52,7 @@ module Hena24
 		end while
 	end function
 
-	iterator function drop_next(ByRef check as Func(of integer,boolean),ByRef _prev as IEnumerable(of integer)) as IEnumerable(of integer)
+	iterator function drop_next(ByVal check as Func(of integer,boolean),ByVal _prev as IEnumerable(of integer)) as IEnumerable(of integer)
 		dim prev as IEnumerator(of integer)=_prev.GetEnumerator()
 		prev.MoveNext()
 		dim a as integer=prev.Current
@@ -69,7 +69,7 @@ module Hena24
 		end while
 	end function
 
-	iterator function drop_n(ByRef check as Func(of integer,integer,boolean),ByRef _prev as IEnumerable(of integer)) as IEnumerable(of integer)
+	iterator function drop_n(ByVal check as Func(of integer,integer,boolean),ByVal n as integer,ByVal _prev as IEnumerable(of integer)) as IEnumerable(of integer)
 		dim prev as IEnumerator(of integer)=_prev.GetEnumerator()
 		dim i as integer=0
 		while true
@@ -83,29 +83,31 @@ module Hena24
 	end function
 
 	sub Main()
-		dim f as new Dictionary(of char,Func(of IEnumerable(of int),IEnumerable(of int))) from {
-			{'S',Function(e) drop_next(is_sq,e)},
-			{'s',Function(e) drop_prev(is_sq,e)},
-			{'C',Function(e) drop_next(is_cb,e)},
-			{'c',Function(e) drop_prev(is_cb,e)},
-			{'h',Function(e) drop_n(is_le,100,e)},
+		dim f as new Dictionary(of char,Func(of IEnumerable(of integer),IEnumerable(of integer))) from {
+			{"S"c,Function(e) drop_next(addressof is_sq,e)},
+			{"s"c,Function(e) drop_prev(addressof is_sq,e)},
+			{"C"c,Function(e) drop_next(addressof is_cb,e)},
+			{"c"c,Function(e) drop_prev(addressof is_cb,e)},
+			{"h"c,Function(e) drop_n(addressof is_le,100,e)}
 		}
-		for i as integer=2 to 9 do
+		for i as integer=2 to 9
 			dim j as integer=i
-			f[ctype('0'+j,char)] = Function(e) drop_n(is_multiple,j,e)
+			f(chr(48+j)) = Function(e) drop_n(addressof is_multiple,j,e)
 		next
-		dim line as string
-		while (line=Console.ReadLine())<>nothing
+		dim line as string=Console.ReadLine()
+		while line isnot nothing
 			dim first as boolean=true
-			'cS => f['S'](f['c'](generate()))
-			for each n as integer in line.Aggregate(generate(),Function(s,e) f[e](s)).Take(10)
+			'cS => f("S"c)(f("c"c)(generate()))
+			for each n as integer in line.Aggregate(generate(),Function(s,e) f(e)(s)).Take(10)
 				if not first
-					Console.Write(',')
+					Console.Write(","c)
 				end if
 				first=false
 				Console.Write(n)
 			next
 			Console.WriteLine()
+			Console.Out.Flush()
+			line=Console.ReadLine()
 		end while
 	end sub
 end module
