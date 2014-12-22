e93431265a64fc487731f6e07fca11fbc04a4879
cielavenir <cielartisan@gmail.com>
2014-12-23 03:18:56 +0900

    rename hackerrank part4

--- hackerrank/divisible-numbers_TLE.rb
+++ hackerrank/divisible-numbers_TLE.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 n=gets.to_i
 (1..1/0.0).each{|i|
-	x=(n*i).to_s.split('').map(&:to_i)
+	x=(n*i).to_s.chars.map(&:to_i)
 	next if x.include?(0)||x.reduce(:+)<x.reduce(:*)
 	p x.size
 	exit
