5f42141c6e86ebc994e6d6939ea5bdafaa6c7661
cielavenir <cielartisan@gmail.com>
2015-01-15 23:53:32 +0900

    remove thisweek_masuipeo

--- codeiq/tyama_codeiq557.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-puts Enumerator.new{|y|
-	a=b=1
-	loop{
-		a,b=a+b,a
-		y<<a if a%a.to_s.chars.map(&:to_i).reduce(:+)==0
-	}
-}.take(11)*',' # 2,3,5,8,21,144,2584,14930352,86267571272,498454011879264,160500643816367088
--- codeiq/tyama_codeiq575.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-(0..9999999).each{|i|
-	if [10,8,2].all?{|e|
-		s=i.to_s(e)
-		s==s.reverse
-	} then p i end
-}
