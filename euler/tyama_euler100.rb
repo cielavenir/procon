3119b54a193cf2ca2bed77dc75dc1d09c1646f21
cielavenir <cielartisan@gmail.com>
2015-09-02 23:35:26 +0900

    euler 100: testing to reformat

--- euler/tyama_euler100.rb
+++ euler/tyama_euler100.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/ruby
-x,y=[1,1]
-while (x+1/2)<10**12 do x,y=[3*x+4*y,2*x+3*y] end
-p (y+1)/2
+gets.to_i.times{
+	p,q,d=gets.split.map(&:to_i)
+	x,y=[1,1]
+	while ((x+p)/q)<d do x,y=[3*x+4*y,2*x+3*y] end # depends on p,q...
+	puts '%d %d'%[(y+p)/q,(x+p)/q]
+}
+__END__
+1
+1 2 1000000000000
