43ef2995c9b3fb2b2ca100f78bc60d367930c3eb
cielavenir <cielartisan@gmail.com>
2015-09-07 20:52:13 +0900

    move codeiq solutions into another repo

--- codeiq/tyama_codeiq161_final.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/ruby
-eval($**"..").map{|n|n%3<1&&x=:Fizz;puts n%5<1?"#{x}Buzz":x||n}
--- codeiq/tyama_codeiq280.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/ruby
-p ('A'..'Z').each_slice(3).to_a.instance_eval{self.shift.zip(*self)}.map(&:join)
