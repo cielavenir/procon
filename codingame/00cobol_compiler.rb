349265ff7cfcf70f1c15635530075359561e4d5e
cielavenir <cielartisan@gmail.com>
2015-10-07 01:17:26 +0900

    add cobol/forth compiler for some reason

---
+++ codingame/00cobol_compiler.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+puts "        identification division."
+puts "        program-id. cobol_compiler."
+puts "        procedure division."
+s=""
+s<<"call \"system\" using \"ruby -e '"
+$<.each{|e|
+	l=e.strip
+	break if l=='__END__'
+	s<<l.gsub('"','""').gsub("'",'""')+';' if !l.start_with?('#')
+}
+s<<"'\"."
+a=s.chars.each_slice(64).with_index.map{|e,i|(i==0?'        ':'      -"')+e.join}
+puts a
+puts "        stop run."
