3ea7eb77c40c3352fa700a8c6bae5a5f81f17a6d
cielavenir <cielartisan@gmail.com>
2015-10-11 03:03:41 +0900

    atcoder 045

---
+++ atcoder/tyama_atcoder045B.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	int N,M;
+	scanf("%d%d",&N,&M);
+	vector<int>v(N+2);
+	vector<pair<int,int> >q(M);
+	for(int i=0;i<M;i++){
+		int x,y;
+		scanf("%d%d",&x,&y);
+		v[x]++,v[y+1]--;
+		q[i].first=x,q[i].second=y;
+	}
+	for(int i=1;i<N+2;i++)v[i]+=v[i-1];
+	for(int i=1;i<N+2;i++)v[i]=v[i-1]+(v[i]==1);
+	vector<int>result;
+	for(int i=0;i<M;i++)if(v[q[i].second]-v[q[i].first-1]==0)result.push_back(i+1);
+	printf("%d\n",result.size());
+	for(int i=0;i<result.size();i++)printf("%d\n",result[i]);
+}
