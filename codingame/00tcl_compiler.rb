43820e813a701086b1b4095390cd7d559d8bd035
cielavenir <cielartisan@gmail.com>
2015-10-15 11:31:27 +0900

    refix tcl stuff

--- codingame/00tcl_compiler.rb
+++ codingame/00tcl_compiler.rb
@@ -4,6 +4,6 @@ print "puts [exec ruby -e \""
 $<.each{|e|
 	l=e.strip
 	break if l=='__END__'
-	print l.gsub('"','\"').gsub('$','\$')+';' if !l.start_with?('#')
+	print l.gsub('"','\"').gsub('$','\$').gsub('[','\[').gsub(']','\]')+';' if !l.start_with?('#')
 }
 puts "\" 2>/dev/null]"
