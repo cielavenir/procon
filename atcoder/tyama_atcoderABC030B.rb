cbaae9590dbdcd7d841f8cac71dd2629dda6b82b
cielavenir <cielartisan@gmail.com>
2015-10-25 01:09:56 +0900

    optimize ABC030B

--- atcoder/tyama_atcoderABC030B.rb
+++ atcoder/tyama_atcoderABC030B.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
-h,m=gets.split.map(&:to_i)
-h=(h%12)*60+m
-r=((Rational(h,720)-Rational(m,60)).abs*360).to_f
-p [r,360-r].min
+h,m=gets.split.map &:to_f
+p [r=(h%12*30-m*5.5).abs,360-r].min
+
+__END__
+h=h%12*60+m
+((h/720-m/60).abs*360).to_f
