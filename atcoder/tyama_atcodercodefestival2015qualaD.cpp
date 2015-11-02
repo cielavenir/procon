49ab66e2349090442bf1ed30d6932cd37fd94696
cielavenir <cielartisan@gmail.com>
2015-11-03 02:40:52 +0900

    now binarysearch() works without overflow

--- atcoder/tyama_atcodercodefestival2015qualaD.cpp
+++ atcoder/tyama_atcodercodefestival2015qualaD.cpp
@@ -9,7 +9,7 @@ using namespace std;
 template<typename T,typename F>
 T binarysearch(T lo,T hi,T eps,const F &checker){
 	for(;lo+eps<hi;){
-		T mi=(lo+hi)/2;
+		T mi=lo+(hi-lo)/2;
 		if(checker(mi)){
 			hi=mi;
 		}else{
@@ -28,15 +28,13 @@ int main(){
 	for(int i=0;i<M;i++)scanf("%d",&X[i]);
 
 #if 0
-	//この挿入句によりbinarysearch()の上限をNに変更できる。
-	//故に(lo+hi)/2でのオーバーフローをint型でも回避できる。
 	if(M==1){
 		printf("%d\n",N-1+min(X[0]-1,N-X[M-1]));
 		return 0;
 	}
 #endif
 
-	printf("%lld\n",1+binarysearch(-1LL,2LL*N,[&](long long n)->bool{
+	printf("%d\n",1+binarysearch(-1,2*N,[&](int n)->bool{
 		int cur=1;
 		for(auto &e:X){
 			if(cur>e)cur=e;
