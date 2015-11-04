b2152ff4acf2c7660bcec34e8e8f7627254d797c
cielavenir <cielartisan@gmail.com>
2015-11-04 18:13:42 +0900

    optimize

--- yukicoder/tyama_yukicoder239.rb
+++ yukicoder/tyama_yukicoder239.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/ruby
 gets
 a=i=0
-p$<.map(&:split).transpose.one?{|e|i+=1;a=i if~e.count('nyanpass')==-e.size}?a:-1
+p$<.map(&:split).transpose.one?{|e|i+=1;~e.count('nyanpass')==-e.size&&a=i}?a:-1
