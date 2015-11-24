e276751266dfb24a0bde4ac1f7017a50025de6df
cielavenir <cielartisan@gmail.com>
2015-11-24 21:20:51 +0900

    more hackerrank

--- hackerrank/eigenvalue-of-the-matrix-4.rb
+++ hackerrank/eigenvalue-of-the-matrix-4.rb
@@ -2,10 +2,10 @@
 require 'matrix'
 v,d,v_inv=Matrix[[2,-1],[-1,2]].inv.eigen
 p d
-v,d,v_inv=Matrix[[2-4,-1-4],[-1-4,2-4]].eigen
+v,d,v_inv=Matrix[[2-4,-1],[-1,2-4]].eigen
 p d
 __END__
 1/3
 1
--7
-3
+-3
+-1
