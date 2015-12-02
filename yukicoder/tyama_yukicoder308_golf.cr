ab857af41a7febe667e2427a949e628b70e3d2f0
cielavenir <cielartisan@gmail.com>
2015-12-03 04:23:42 +0900

    yukicoder 308 golf

---
+++ yukicoder/tyama_yukicoder308_golf.cr
@@ -0,0 +1,5 @@
+#!/usr/bin/env crystal
+require "big_int"
+h={4=>3,6=>5,12=>11,14=>13,18=>8,20=>19,21=>19,24=>23,25=>23}
+n=BigInt.new(gets.not_nil!)
+p n<26 ? h.has_key?(n) ? h[n] : 7 : n%8+`factor #{n-8}`.count(' ')==2 ? 14 : 8
