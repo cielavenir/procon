41fa14bf60c5e211c3c79a0df19df2e77c34a66f
cielavenir <cielartisan@gmail.com>
2016-02-04 23:11:17 +0900

    update codingame temperatures

--- codingame/temperatures.rb
+++ codingame/temperatures.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/ruby
-N = gets.to_i
-if N==0
-	p 0
-else
-	a=gets.split.map(&:to_i)
-	if a.all?{|e|e<0}
-		p a.max
-	else
-		p a.map(&:abs).min
-	end
-end
+gets;p gets.split.map(&:to_i).min_by{|e|[e.abs,-e]}||0
