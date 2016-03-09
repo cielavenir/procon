10719ba10e95674ac6d9dad3159348a0d61d9c05
cielavenir <cielartisan@gmail.com>
2016-03-09 18:46:35 +0900

    RUPC2016bB AC

--- aizu/tyama_aizuRUPC2016bB.cpp
+++ aizu/tyama_aizuRUPC2016bB.cpp
@@ -6,7 +6,7 @@ const int D[3][2]={{1,0},{0,1},{-1,0}};
 int main(){
 	int W,H,N,d,r=0;
 	scanf("%d%d%d",&W,&H,&N);
-	vector<vector<int>> v={{W,H,W},{W-1,H-2,W-1}}, p={{0,0},{0,1}}, c={{0,0},{0,0}};
+	vector<vector<int>> v={{W-1,H-1,W-1},{W-2,H-3,W-2}}, p={{0,0},{0,1}}, c={{0,0},{0,0}};
 	for(;N--;){
 		scanf("%d",&d);
 		p[d][0]+=D[c[d][0]][0];
