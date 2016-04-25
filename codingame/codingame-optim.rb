f00451659c3ac69c711e1bab160a29a235b56fdb
cielavenir <cielartisan@gmail.com>
2016-04-25 16:13:28 +0900

    more codingame

--- codingame/codingame-optim.rb
+++ codingame/codingame-optim.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby
 STDOUT.sync=true
 A=3.times.map{gets.to_i}
+STDERR.puts A.inspect
 loop{
 	a=4.times.map{gets.chomp}
-	b=gets.split.map(&:to_i)
+	b=5.times.map{gets.split.map(&:to_i)}
 	STDERR.puts a.inspect+' '+b.inspect
     
-    puts "C"
+    #puts a.count{|e|e=='#'}>1 ? "C" : "C"
+    puts a[0]=='#' ? "A" : "C"
 }
---
+++ codingame/tensorflow.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# MNIST For ML Beginners!
+
+# This tutorial is intended for readers who are new to both machine learning and TensorFlow.
+# Just like programming has Hello World, machine learning has MNIST.
+# MNIST is a simple computer vision dataset. It consists of images of handwritten digits.
+# Source: https://goo.gl/B14py7
+
+# Please help us to improve this section by sending us your
+# feedbacks and comments on: https://docs.google.com/forms/d/16fH20Qf8gJ2o31Vnlss2uLJ7wL9vq76TeUGqghTY0uI/viewform
+
+# Importing input data
+import random
+import input_data
+mnist = input_data.read_data_sets(raw_input(), raw_input(), raw_input())
+
+import tensorflow as tf
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+for i in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+result = sess.run(tf.argmax(y,1), feed_dict={x: mnist.validation.images})
+print ' '.join(map(str, result))
