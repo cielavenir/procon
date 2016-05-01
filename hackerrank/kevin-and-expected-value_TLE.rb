16459282aed68f398ee50cb4d2d6d9c2d50cc58a
cielavenir <cielartisan@gmail.com>
2016-05-02 01:14:13 +0900

    update hackerrank

--- hackerrank/kevin-and-expected-value_TLE.rb
+++ hackerrank/kevin-and-expected-value_TLE.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/ruby
-y=lambda{|x|5.times.reduce(0){|s,_|Math.sqrt(x+s)}}
+#y=lambda{|x|10.times.reduce(0){|s,_|Math.sqrt(x+s)}}
+y=lambda{|x|x==0 ? 0 : (1+Math.sqrt(1+4*x))/2}
 n=5000000
-a=n.times.map(&y)
+a=[0]+(1...n).map(&y)
 (1...n).each{|i|a[i]+=a[i-1]}
 gets.to_i.times{
 	m=gets.to_i
-	p a[m-1]/m
+	if m<=n
+		p a[m-1]/m
+	else
+=begin
+		l=1000
+		s=0
+		l.times{
+			s+=y[rand(0...m)]
+		}
+		p s/l
+=end
+		p -1
+	end
 }
