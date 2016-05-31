6d378122a495c6398ad0e0d798ed8f58314cd55e
cielavenir <cielartisan@gmail.com>
2016-05-31 11:28:21 +0900

    hackerrank/validating-postalcode: removed if completely

--- hackerrank/validating-postalcode.py
+++ hackerrank/validating-postalcode.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from __future__ import print_function
 import re,sys
-if sys.version_info[0]>=3: raw_input=input
 
-s=raw_input().strip()
+s=sys.stdin.readline().strip()
 print(bool(re.search(r'^[1-9][0-9]{5}$',s) and len(re.findall(r'(.)(?=.\1)',s))<2))
