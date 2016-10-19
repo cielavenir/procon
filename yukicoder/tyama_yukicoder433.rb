ded29355c3e5223f3250fde3e8fe351bc7b78ad9
cielavenir <cielartisan@gmail.com>
2016-10-20 02:52:26 +0900

    optimized yukicoder 433

--- yukicoder/tyama_yukicoder433.rb
+++ yukicoder/tyama_yukicoder433.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/ruby
-h=Hash.new 0;n,k=gets.split.map &:to_i;p *n.times.map{|i|s,t,u=gets.split.map &:to_i;[-s,u,t,i]}.sort.map{|e|e[1]=h[e[1]]+=1;e}.sort.map(&:last)[0,k]
+f=->{gets.split.map &:to_i};h=Hash.new 0;n,k=f[];p *n.times.map{|i|s,t,u=f[];[-s,u,t,i]}.sort.sort_by{|e|e[1]=h[e[1]]+=1;e}.map(&:last)[0,k]
