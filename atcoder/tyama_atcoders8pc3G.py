1cb452af5fc6c70ba87968161cf06eaed7298c58
cielavenir <cielartisan@gmail.com>
2016-11-23 01:44:02 +0900

    optimized atcoder s8pc3G

--- atcoder/tyama_atcoders8pc3G.py
+++ atcoder/tyama_atcoders8pc3G.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 def f(n):
 	e=[1,0,0,1];z=[1,1,1,0]
-	while n:
-		if n%2>0:e=g(e,z)
-		z=g(z,z);n//=2
+	while n:e=[e,g(e,z)][n%2];z=g(z,z);n//=2
 	return e[1]
-M=998244353;g=lambda a,b:[(a[0]*b[0]+a[1]*b[2])%M,(a[0]*b[1]+a[1]*b[3])%M,(a[2]*b[0]+a[3]*b[2])%M,(a[2]*b[1]+a[3]*b[3])%M];n,m=map(int,input().split());I=[1,1];r=f(m+2*n-2);c=1
+M=998244353;g=lambda a,b:[(a[x]*b[y]+a[z]*b[w])%M for x,y,z,w in[[0,0,1,2],[0,1,1,3],[2,0,3,2],[2,1,3,3]]];n,m=map(int,input().split());I=[1,1];r=f(m+2*n-2);c=1
 for i in range(2,n):I+=[(M-M//i)*I[M%i]%M]
 for i in range(n-1):r=(r+c*(M-f(2*n-2-2*i)))%M;c=c*(m+i)*I[i+1]%M
 print(r)
