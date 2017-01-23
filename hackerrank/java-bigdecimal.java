edf7d4414f64344d6bf6cf3bacf73b4480d6890e
cielavenir <cielartisan@gmail.com>
2017-01-23 22:27:19 +0900

    hackerrank: updated answer format

--- hackerrank/java-bigdecimal.java
+++ hackerrank/java-bigdecimal.java
@@ -1,17 +1,16 @@
 import java.util.*;
 import java.math.*;
-import javafx.util.Pair;
 
 class Solution{
 	public static void main(String[] args){
 		Scanner cin=new Scanner(System.in);
 		int T=cin.nextInt();
-		ArrayList<Pair<BigDecimal,String> > a=new ArrayList<Pair<BigDecimal,String> >();
+		ArrayList<javafx.util.Pair<BigDecimal,String> > a=new ArrayList<javafx.util.Pair<BigDecimal,String> >();
 		for(int i=0;i<T;i++){
 			String s=cin.next();
-			a.add(new Pair<BigDecimal,String>(new BigDecimal(s),s));
+			a.add(new javafx.util.Pair<BigDecimal,String>(new BigDecimal(s),s));
 		}
-		Collections.sort(a,(x,y)->x.getKey().compareTo(y.getKey()));
-		for(int i=T-1;i>=0;i--)System.out.println(a.get(i).getValue());
+		Collections.sort(a,(x,y)->-x.getKey().compareTo(y.getKey()));
+		for(int i=0;i<T;i++)System.out.println(a.get(i).getValue());
 	}
 }
