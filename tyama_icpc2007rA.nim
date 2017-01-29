556c0e284c0c5acff7c5b077c0ccdaaf711316c6
cielavenir <cielartisan@gmail.com>
2017-01-30 02:00:07 +0900

    update nim shebang

--- tyama_icpc2007rA.nim
+++ tyama_icpc2007rA.nim
@@ -1,4 +1,6 @@
-#!/usr/bin/env nim
+#[
+/usr/bin/env nim c --nimcache:$HOME/.nimcache --opt:speed --run $0 $@;exit
+#]#
 import strutils
 while true:
  var NKM=readLine(stdin).split()
--- tyama_icpc2012dA.nim
+++ tyama_icpc2012dA.nim
@@ -1,4 +1,6 @@
-#!/usr/bin/env nim
+#[
+/usr/bin/env nim c --nimcache:$HOME/.nimcache --opt:speed --run $0 $@;exit
+#]#
 import strutils
 var n=parseInt(readLine(stdin))
 for i in 1..n:
