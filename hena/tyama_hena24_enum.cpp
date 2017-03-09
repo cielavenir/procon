ec702c40120d7b0dad2e087e0b980da247884803
cielavenir <cielartisan@gmail.com>
2017-03-10 00:38:30 +0900

    hena24 Perl/C++17

---
+++ hena/tyama_hena24_enum.cpp
@@ -0,0 +1,97 @@
+// http://qiita.com/Nabetani/items/1c83005a854d2c6cbb69
+// http://nabetani.sakura.ne.jp/hena/ord24eliseq/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+#include <experimental/generator>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+using namespace std::experimental;
+
+generator<int> generate(){
+	int i=1;
+	for(;;){
+		co_yield i;
+		i+=1;
+	}
+}
+generator<int> drop_prev(const function<bool(int)> &check,const generator<int> &_prev){
+	auto prev=_prev.begin();
+	int a=*prev;
+	++prev;
+	int b=*prev;
+	for(;;){
+		if(!check(b))co_yield a;
+		a=b;
+		++prev;
+		b=*prev;
+	}
+}
+generator<int> drop_next(const function<bool(int)> &check,const generator<int> &_prev){
+	auto prev=_prev.begin();
+	int a=*prev;
+	++prev;
+	int b=*prev;
+	co_yield a;
+	for(;;){
+		if(!check(a))co_yield b;
+		a=b;
+		++prev;
+		b=*prev;
+	}
+}
+generator<int> drop_n(const function<bool(int,int)> &check,int n,const generator<int> &_prev){
+	auto prev=_prev.begin();
+	int i=0;
+	for(;;){
+		i++;
+		int a=*prev;
+		if(!check(i,n))co_yield a;
+		++prev;
+	}
+}
+bool is_sq(int n){
+	int x=(int)sqrt(n);
+	return x*x==n;
+}
+bool is_cb(int n){
+	int x=(int)cbrt(n);
+	return x*x*x==n;
+}
+bool is_multiple(int i,int n){return i%n==0;}
+bool is_le(int i,int n){return i<=n;}
+
+int main(){
+	map<char,function<generator<int>(const generator<int>&)>> f={
+		{'S',[&](auto &e){return drop_next(&is_sq,e);}},
+		{'s',[&](auto &e){return drop_prev(&is_sq,e);}},
+		{'C',[&](auto &e){return drop_next(&is_cb,e);}},
+		{'c',[&](auto &e){return drop_prev(&is_cb,e);}},
+		{'h',[&](auto &e){return drop_n(&is_le,100,e);}},
+	};
+	for(int i=2;i<10;i++){
+		f[(char)('0'+i)] = [&](auto &e){return drop_n(&is_multiple,i,e);};
+	}
+	string line;
+	for(;getline(cin,line);){
+		bool first=true;
+		//cS => f['S'](f['c'](generate()))
+		//auto z=reduce!((s,e)=>f[e](s))(generate(),line);
+
+		auto z=generate();
+		for(char e:line)z=f[e](z);
+		auto it=z.begin();
+		for(int i=0;i<10;i++){
+			int n=*it;
+			++it;
+			if(!first)putchar(',');
+			first=false;
+			printf("%d",n);
+		}
+		puts("");
+		fflush(stdout);
+	}
+}
---
+++ hena/tyama_hena24_enum.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/env perl
+#http://qiita.com/Nabetani/items/1c83005a854d2c6cbb69
+#http://nabetani.sakura.ne.jp/hena/ord24eliseq/
+
+use strict vars;
+use feature qw(say);
+use IO::Handle;
+use Generator::Object;
+use List::Util qw(reduce);
+
+sub cbrt{
+	my($n)=@_;
+	return $n**(1-2.0/3);
+}
+
+sub generate{
+	generator{
+		my $i=1;
+		for(;;){
+			$_->yield($i);
+			$i++;
+		}
+	}
+}
+
+sub drop_prev{
+	my($check,$prev)=@_;
+	generator{
+		my $a=$prev->next;
+		my $b=$prev->next;
+		for(;;){
+			$_->yield($a) if !$check->($b);
+			$a=$b;
+			$b=$prev->next;
+		}
+	}
+}
+sub drop_next{
+	my($check,$prev)=@_;
+	generator{
+		my $a=$prev->next;
+		my $b=$prev->next;
+		$_->yield($a);
+		for(;;){
+			$_->yield($b) if !$check->($a);
+			$a=$b;
+			$b=$prev->next;
+		}
+	}
+}
+sub drop_n{
+	my($check,$n,$prev)=@_;
+	generator{
+		my $i=0;
+		for(;;){
+			$i+=1;
+			$a=$prev->next;
+			$_->yield($a) if !$check->($i,$n);
+		}
+	}
+}
+sub is_sq{
+	my($n)=@_;
+	my $x=int(sqrt($n));
+	return $x*$x==$n;
+}
+sub is_cb{
+	my($n)=@_;
+	my $x=int(cbrt($n));
+	return $x*$x*$x==$n;
+}
+sub is_multiple{
+	my($i,$n)=@_;
+	return $i%$n==0;
+}
+sub is_le{
+	my($i,$n)=@_;
+	return $i<=$n;
+}
+
+my %f=(
+	'S'=>sub{my($enum)=@_;return drop_next('is_sq',$enum);},
+	's'=>sub{my($enum)=@_;return drop_prev('is_sq',$enum);},
+	'C'=>sub{my($enum)=@_;return drop_next('is_cb',$enum);},
+	'c'=>sub{my($enum)=@_;return drop_prev('is_cb',$enum);},
+	'h'=>sub{my($enum)=@_;return drop_n('is_le',100,$enum);},
+);
+for(my $i=2;$i<=9;$i++){$f{"$i"}=sub{my($i)=@_;sub{my($enum)=@_;return drop_n('is_multiple',$i,$enum);}}->($i);}
+
+STDOUT->autoflush;
+for(;my $line=<>;){
+	chomp($line);
+	#cS => $f['S']->($f['c']->(generate()))
+	my $z=reduce {$f{$b}->($a)} generate(),split('',$line);
+	my $cnt=10;
+	my @a;
+	for(my $i=0;$i<$cnt;$i++){
+		push(@a,$z->next);
+	}
+	say join(',',@a);
+}
