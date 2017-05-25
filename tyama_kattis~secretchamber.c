4d84f719f70a37b4c989aecef5f9dad35f4d86d6
cielavenir <cielartisan@gmail.com>
2017-05-25 14:30:28 +0900

    ICPC final E/I

---
+++ tyama_kattis~secretchamber.c
@@ -0,0 +1,27 @@
+//C99 or C++98
+#include <stdio.h>
+#include <string.h>
+
+int a[26][26];
+char s[99],t[99];
+int main(){
+    const int INF=1<<29;
+    int m,n;
+    for(int i=0;i<26;i++)for(int j=0;j<26;j++)a[i][j]=(i!=j ? INF : 0);
+    for(scanf("%d%d",&m,&n);m--;){
+        scanf("%s%s",s,t);
+        a[*s-'a'][*t-'a']=1;
+    }
+    for(int k=0;k<26;k++)for(int i=0;i<26;i++)for(int j=0;j<26;j++){
+        if(a[i][j]>a[i][k]+a[k][j])a[i][j]=a[i][k]+a[k][j];
+    }
+    for(;n--;){
+        scanf("%s%s",s,t);
+        if(strlen(s)!=strlen(t))puts("no");
+        else{
+            int i=0;
+            for(;i<strlen(s)&&a[s[i]-'a'][t[i]-'a']<INF;i++);
+            puts(i<strlen(s) ? "no" : "yes");
+        }
+    }
+}
---
+++ tyama_kattis~speed.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <cstdio>
+using namespace std;
+
+// cpp_binarysearch (C) @cielavenir under Boost Software License.
+// returns the smallest value satisfying the predicate.
+// type F should be something like std::function<bool(T)>.
+
+template<typename T,typename F>
+T binarysearch(T lo,T hi,const T eps,const F &predicate){
+	T r=hi+eps;
+	for(;lo<hi+eps;){
+		T mi=eps!=1||(lo<0)==(hi<0) ? lo+(hi-lo)/2 : (lo<-hi) ? -((-lo-hi-1)/2+1) : (lo+hi)/2;
+		if(predicate(mi)){
+			r=mi;
+			hi=mi-eps;
+		}else{
+			lo=mi+eps;
+		}
+	}
+	return r;
+}
+template<typename T,typename F>
+T binarysearch(T lo,T hi,const F &predicate){return binarysearch(lo,hi,(T)1,predicate);}
+
+template<typename F>
+double binarysearch(double _lo,double _hi,const F &predicate){
+	long long lo;
+	if(_lo<0)_lo=-_lo,lo=-*(long long*)(&_lo);else lo=*(long long*)(&_lo);
+	long long hi;
+	if(_hi<0)_hi=-_hi,hi=-*(long long*)(&_hi);else hi=*(long long*)(&_hi);
+	long long _r=binarysearch(lo,hi,1LL,[&](long long _mi){
+		double mi;
+		if(_mi<0)_mi=-_mi,mi=-*(double*)(&_mi);else mi=*(double*)(&_mi);
+		return predicate(mi);
+	});
+	double r;
+	if(_r<0)_r=-_r,r=-*(double*)(&_r);else r=*(double*)(&_r);
+	return r;
+}
+
+int main(){
+    int n;
+    double t,mi=1e9;
+    scanf("%d%lf",&n,&t);
+    vector<pair<double,double>>v(n);
+    for(int i=0;i<n;i++){
+        scanf("%lf%lf",&v[i].first,&v[i].second);
+        if(mi>v[i].second)mi=v[i].second;
+    }
+    printf("%f\n",binarysearch(-mi+1e-9,1e7,[&](double c)->bool{
+        double r=0;
+        for(auto &e:v){
+            r+=e.first/(e.second+c);
+        }
+        return t>=r;
+    }));
+}
