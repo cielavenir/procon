8a4dacadc2b1c103a2286b54e00ada89126b0a84
cielavenir <cielartisan@gmail.com>
2017-08-06 02:41:33 +0900

    hena dumper PicoC

---
+++ hena/hena_dumper.c
@@ -0,0 +1,6 @@
+//usr/bin/env picoc -s $0 $@;exit
+//usage: hena_dumper.c testdata.txt 1>input.txt 2>input.txt
+void test(char *in,char *out){
+	fprintf(stdout,"%s\n",in);
+	fprintf(stderr,"%s\n",out);
+}
