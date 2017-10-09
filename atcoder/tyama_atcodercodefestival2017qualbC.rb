2578434feeadb5b65781df0e745b4ae99d599f4c
cielavenir <cielartisan@gmail.com>
2017-10-10 00:44:43 +0900

    optimized atcodercodefestival2017qualbC

--- atcoder/tyama_atcodercodefestival2017qualbC.rb
+++ atcoder/tyama_atcodercodefestival2017qualbC.rb
@@ -1,19 +1,6 @@
 #!/usr/bin/ruby
-ENV[Z='RUBY_THREAD_VM_STACK_SIZE']||exec({Z=>?5*8},'ruby',$0)
-def dfs(c)
-	H[c].each{|e|
-	if Z[e]
-		raise if (Z[c]-Z[e])%2<1
-	else
-		Z[e]=Z[c]+1
-		dfs(e)
-	end
-	}
-end
-N,M=gets.split.map &:to_i
-H=Hash.new{|h,k|h[k]=[]};Z={1=>0}
-M.times{
-a,b=gets.split.map &:to_i
-H[a]<<b;H[b]<<a
-}
-p _=(dfs(1)&&Z.partition{|k,v|v%2<1}.map(&:size).reduce(:*) rescue N*~-N/2)-M
+ENV[A='RUBY_THREAD_VM_STACK_SIZE']||exec({A=>?5*8},'ruby',$0)
+F=->c{H[c].all?{|e|Z[e]?Z[c]^Z[e]>0:(Z[e]=1^Z[c];F[e])}}
+N,M=gets.split.map &:to_i;H=Hash.new{|h,k|h[k]=[]};Z={1=>0}
+$<.map{|e|a,b=e.split.map &:to_i;H[a]<<b;H[b]<<a}
+p (F[1]?(x,y=Z.partition{|k,v|v>0};x.size*y.size):N*~-N/2)-M
