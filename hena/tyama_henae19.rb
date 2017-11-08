3edb935817f0eeef527d5ddfadef51ceb55b826e
cielavenir <cielartisan@gmail.com>
2017-11-09 01:23:50 +0900

    hena e19

--- hena/tyama_henae19.rb
+++ hena/tyama_henae19.rb
@@ -2,21 +2,40 @@
 #https://mtsmfm.github.io/2017/11/04/doukaku-e19.html
 #http://qiita.com/mtsmfm/items/67bf5c121ecbd9b5fab3
 
-#o -> odd, x->even
+T={'o'=>:odd?,'x'=>:even?}
 
 def enum(a,d,s,n)
-	return if s>=n
+	(yield [s];return) if d==n-1
+	s0=s
 	if a[d]==a[d+1]
-		#if s
-		s+1
+		s+=1
+		s+=2 if s%2==0
+		enum(a,d+1,s,n){|e|yield [s0]+e} if s<2*n
+		((s+4)/2*2).step(2*n-1,4){|i|
+			enum(a,d+1,i,n){|e|yield [s0]+e}
+		}
 	else
-		s=s/2*2
+		s=s/2*2+2
+		s.step(2*n-1,4){|i|
+			enum(a,d+1,i,n){|e|yield [s0]+e}
+		}
 	end
 end
 
-
 while gets
 	x,y=$_.chomp.split(',')
-	enum(x,x.size){}
+	n=x.size
+	s=x[0]=='o'?0:2
+	r=[]
+	s.step(2*n-1,4){|i|
+		enum(x,0,i,n){|e|
+			b=([*0...2*n]-e).map{|z|z/2+1}
+			a=e.map{|z|z/2+1}
+			if y.chars.zip(b).all?{|_1,_2|_2.send T[_1]}
+				r<<[a,b]*','
+			end
+		}
+	}
+	puts r*'|'
 	STDOUT.flush
 end
