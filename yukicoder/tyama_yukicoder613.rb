a6277b1779e4f9527b088bcf5ef1207ac3cdab0a
cielavenir <cielartisan@gmail.com>
2017-12-14 08:34:42 +0900

    yukicoder 613

---
+++ yukicoder/tyama_yukicoder613.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#cf: codeiq 3130
+def pow(x,y,m)
+	z=1
+	while y>0
+		z=z*x%m if y%2>0
+		x=x*x%m
+		y/=2
+	end
+	z
+end
+k,M=gets.split.map &:to_i
+k=pow(2,k,M+(pow(3,M/2,M)==1?-1:1))
+def mul(a,b)
+	[(a[0]*b[0]+3*a[1]*b[1])%M,(a[1]*b[0]+b[1]*a[0])%M]
+end
+x=[2,1]
+e=[1,0]
+while k>0
+	e=mul(e,x) if k%2>0
+	x=mul(x,x)
+	k/=2
+end
+p (2*e[0]-2)%M
