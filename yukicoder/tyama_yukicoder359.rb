52a21aa299934170b769fe4ff0aaa2e09a09bcf4
cielavenir <cielartisan@gmail.com>
2018-01-18 00:06:26 +0900

    yukicoder: fix proc passing

--- yukicoder/tyama_yukicoder359.rb
+++ yukicoder/tyama_yukicoder359.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/ruby
-f=->a,b,c{a<c&&((b<a&&b<c)||(b>a&&b>c))}
+#Ruby 2.2.0 - 2.4.0: lambda was OK (2.4.1 accepts only proc)
+#f=->a,b,c{a<c&&((b<a&&b<c)||(b>a&&b>c))}
+f=proc{|a,b,c|a<c&&((b<a&&b<c)||(b>a&&b>c))}
 $><<gets.split.map(&:to_i).permutation.any?{|e|e.each_cons(3).all?(&f)}?:YES: :NO
--- yukicoder/tyama_yukicoder623.rb
+++ yukicoder/tyama_yukicoder623.rb
@@ -1,2 +1,3 @@
+#!/usr/bin/ruby
 r,*c,_,a=$<.map{|e|e.split.map &:to_i}
 a.map{|e|f=1,e;c.map{|t,x,y|f<<r=[x*y=f[y],y*x=f[x],x+y][t-2]%998244353};p r}
