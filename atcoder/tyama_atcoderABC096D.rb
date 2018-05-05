4eac6103a85c9190426f051b6234a0627955fa8a
cielavenir <cielartisan@gmail.com>
2018-05-06 02:33:57 +0900

    optimized abc096d

--- atcoder/tyama_atcoderABC096D.rb
+++ atcoder/tyama_atcoderABC096D.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/ruby
+i=3;gets.to_i.times{i+=5;2**i%i==2?(p i):redo}
+__END__
 require'prime';p *Prime.each(1499).select{|e|e%5>3}[0,gets.to_i]
+require'prime';p *3.step(1499,5).select(&:prime?)[0,gets.to_i]
+#https://abc096.contest.atcoder.jp/submissions/2465874
+p *3.step(99999,5).select{|i|2**i%i==2}[0,gets.to_i]
--- atcoder/tyama_atcoderABC096D_mod.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/ruby
-#https://abc096.contest.atcoder.jp/submissions/2465874
-p *3.step(1499,5).select{|i|2**i%i==2}[0,gets.to_i]
