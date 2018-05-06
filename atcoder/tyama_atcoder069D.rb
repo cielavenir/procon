30c527a2e1200b24556736ff59392dd53deb3b0a
cielavenir <cielartisan@gmail.com>
2018-05-07 00:02:13 +0900

    optimize rb p

--- atcoder/tyama_atcoder069D.rb
+++ atcoder/tyama_atcoder069D.rb
@@ -3,4 +3,4 @@ n,s=`dd`.split;n=n.to_i
 %w(SS SW WS WW).find{|a|
 	2.upto(n+1){|i|a[i]=(a[i-1]==?W)^(a[i-2]==?W)^(s[~-i%n]==?o)??S:?W}
 	a[0,2]==a[n,2]&&$><<a[0..-3]*''
-}||(p -1)
+}||p(-1)
--- atcoder/tyama_atcoderABC096D.rb
+++ atcoder/tyama_atcoderABC096D.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-i=3;gets.to_i.times{i+=5;2**i%i==2?(p i):redo}
+i=3;gets.to_i.times{i+=5;2**i%i==2?p(i):redo}
 __END__
 require'prime';p *Prime.each(1499).select{|e|e%5>3}[0,gets.to_i]
 require'prime';p *3.step(1499,5).select(&:prime?)[0,gets.to_i]
