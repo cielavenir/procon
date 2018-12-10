a0a47bd73481ae2a203040a424be70b4751c82bb
cielavenir <cielartisan@gmail.com>
2018-12-10 14:57:06 +0900

    update kt

--- codeforces/tyama_codeforces683C.kt
+++ codeforces/tyama_codeforces683C.kt
@@ -1,30 +1,30 @@
-import java.util.*
+import kotlin.collections.*
 fun main(args: Array<String>){
-	var i=0
-	var x=0
-	val cin=Scanner(System.`in`)
-	val t=TreeMap<Int,Int>()
-	var na=cin.nextInt()
-	while(na>0){
-		x=cin.nextInt()
+	val t=HashMap<Int,Int>()
+	var a=readLine()!!.split(" ").map(String::toInt).toMutableList()
+	a.removeAt(0)
+	for(x in a){
 		if(!t.containsKey(x))t.put(x,0)
 		t.put(x,t.get(x)!!+1)
-		na-=1
 	}
-	var nb=cin.nextInt()
-	while(nb>0){
-		x=cin.nextInt()
+	a=readLine()!!.split(" ").map(String::toInt).toMutableList()
+	a.removeAt(0)
+	for(x in a){
 		if(!t.containsKey(x))t.put(x,0)
 		t.put(x,t.get(x)!!-1)
-		nb-=1
 	}
 	var n=0
 	for(m in t){
 		if(m.value!=0)n+=1
 	}
 	print(n)
+	val x=mutableListOf<Int>()
 	for(m in t){
-		if(m.value!=0)print(" "+m.key)
+		if(m.value!=0)x.add(m.key)
+	}
+	x.sort()
+	for(m in x){
+		print(" "+m)
 	}
 	println()
 }
