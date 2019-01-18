d36b517a8894122bf2797342aa0da7f85cb9a2ac
cielavenir <cielartisan@gmail.com>
2019-01-19 01:11:47 +0900

    fix cf1101B

--- codeforces/tyama_codeforces1101B.rb
+++ codeforces/tyama_codeforces1101B.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/ruby
+s=gets
+begin
+idx=s.index('[')
+s=s[idx+1..-1]
+idx=s.index(':')
+s=s[idx+1..-1]
+idx=s.rindex(']')
+s=s[0...idx]
+idx=s.rindex(':')
+s=s[0...idx]
+p s.count('|')+4
+rescue
+p -1
+end
+__END__
 p gets=~/\[.*?\:(.*)\:.*?\]/ ? $1.count(?|)+4 : -1
