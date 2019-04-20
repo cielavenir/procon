0e9a5ad8e161f095430b5da37ebe6f073af1191b
cielavenir <cielartisan@gmail.com>
2019-04-20 15:10:17 +0900

    optimized yukicoder 811

--- yukicoder/tyama_yukicoder761.rb
+++ yukicoder/tyama_yukicoder761.rb
@@ -2,5 +2,5 @@
 n,*a=`dd`.split.map &:to_i
 *s=0
 n.times{|i|s<<s[i]+a[i]}
-f=->l,r{a[l]==a[r-1]||!f[l,t=a.bsearch_index{|e|e>=(s[r]-s[l]+r-l-1)/(r-l)}]||!f[t,r]}
+f=->l,r{a[l]==a[r-1]||!f[l,t=a.bsearch_index{|e|e>=(s[r]-s[l]+r+~l)/(r-l)}]||!f[t,r]}
 puts f[0,n]?:First: :Second
--- yukicoder/tyama_yukicoder811.rb
+++ yukicoder/tyama_yukicoder811.rb
@@ -1,18 +1,11 @@
 #!/usr/bin/ruby
 require'prime'
 n,k=gets.split.map &:to_i
-r=rn=0
+r=t=0
 o=Hash[n.prime_division]
-(2...n).each{|i|
-	q=i.prime_division
-	k0=k
-	q.each{|n,p|k0-=[o[n].to_i,p].min}
-	if k0<=0
-		r0=q.map{|n,p|p+1}.reduce(:*)
-		if r<r0
-			r=r0
-			rn=i
-		end
-	end
+(2...n).map{|i|
+	s=1
+	l=i.prime_division.sum{|n,p|s*=p+1;[o[n]||0,p].min}
+	k<=l&&r<s&&(r=s;t=i)
 }
-p rn
+p t
