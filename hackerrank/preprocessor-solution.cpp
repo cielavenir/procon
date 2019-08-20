c9023b9a45008fedbfeef01cb25ff595cd3f580b
Kübra Kutlu <kubrakutlu9@gmail.com>
2019-08-20 18:08:30 +0300

    Update #define FUNCTION(f,q) and #define INF

--- hackerrank/preprocessor-solution.cpp
+++ hackerrank/preprocessor-solution.cpp
@@ -1,7 +1,7 @@
 #include <climits>
-#define INF INT_MAX
 #define io(v) cin>>v
-#define FUNCTION(f,q) void f(int &a,int &b){a=(a)q(b)?a:b;}
+#define FUNCTION(f,q) void f(int &a,int b){int *ptra=&a; if(b q *ptra) *ptra=b;}
+#define INF -10^8
 #define toStr(s) #s
 #define foreach(a,b) for(int b=0;b<a.size();b++)
 
