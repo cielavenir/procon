969294a43a0384cc8dcba97e46eeabb1e3e66137
cielavenir <cielartisan@gmail.com>
2020-08-23 13:58:31 +0900

    fix yukicoder 757 cr

--- yukicoder/tyama_yukicoder757.cr
+++ yukicoder/tyama_yukicoder757.cr
@@ -1,5 +1,7 @@
 #!/usr/bin/env crystal
-require "big/big_int"
+lib C;fun strtoll(s: UInt8*,p: UInt8**,b: Int32): Int64;end
+class String;def to_i64;C.strtoll(self,nil,10);end;end
+require "big"
 starting=1
 base=BigInt.new(gets.not_nil!)
 while (s=gets)!=nil
