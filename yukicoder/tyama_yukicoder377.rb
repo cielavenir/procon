88a4ec3e1c8a627b30e784a7e7497cadadf308c4
cielavenir <cielartisan@gmail.com>
2021-06-15 00:23:49 +0900

    maintained yukicoder 377

--- yukicoder/tyama_yukicoder377.rb
+++ yukicoder/tyama_yukicoder377.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/ruby
+def yield_from(*funcsyms)
+	funcsyms.each{|funcsym|
+		func = method(funcsym)
+		define_method(funcsym){|*args,&blk|
+			return to_enum(funcsym,*args) if !blk
+			func.(*args){|*a|a.each{|e|blk.(e)}}
+		}
+	}
+end
+
 if false
 	require 'prime'
 	def prime_division(n) n.prime_division end
@@ -15,18 +25,18 @@ end
 def divisor_totient(a,d,n,t)
 	return to_enum(:divisor,a,d,n,t) if !block_given?
 	if d==a.size
-		yield n,t
+		yield [n,t]
 	else
 		(0..a[d][1]).each{|i|
-			divisor_totient(
+			yield *divisor_totient(
 				a,d+1,
 				n*a[d][0]**i,
-				i==0 ? t : t*(a[d][0]-1)*a[d][0]**(i-1),
-				&proc
+				i==0 ? t : t*(a[d][0]-1)*a[d][0]**(i-1)
 			)
 		}
 	end
 end
+yield_from :divisor_totient
 def pow(x,y,m)
 	z=1
 	while y>0
--- yukicoder/tyama_yukicoder377_golf.rb
+++ yukicoder/tyama_yukicoder377_golf.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/ruby
 require'prime'
-def x(a,d=0,n=1,t=1)d==a.size ?yield(n,t):(0..a[d][1]).map{|i|x(a,d+1,n*a[d][0]**i,i>0?t*~-a[d][0]*a[d][0]**~-i:t,&proc)}end
+def x(a,d=0,n=1,t=1,&b)d==a.size ? b.(n,t):(0..a[d][1]).map{|i|x(a,d+1,n*a[d][0]**i,i>0?t*~-a[d][0]*a[d][0]**~-i:t,&b)}end
 q=->x,y{z=1;(y%2>0&&z=z*x%M;x=x*x%M;y/=2)while y>0;z}
 C={}
 r=M=10**9+7
 H,W,K=gets.split.map &:to_i
-Z=:prime_division
-x(H.send(Z)){|a,c|x(W.send(Z)){|b,d|r=(r+c*d*C[k=W*H/a.lcm(b)]||=q[K,k])%M}}
+x(H.prime_division){|a,c|x(W.prime_division){|b,d|r=(r+c*d*C[k=W*H/a.lcm(b)]||=q[K,k])%M}}
 p r*q[W*H,M-2]%M
