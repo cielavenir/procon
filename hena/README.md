ed3846afa8b327a159ef21a711571eb52278bb73
cielavenir <cielartisan@gmail.com>
2022-10-26 09:32:05 +0900

    add yokohamarb103

---
+++ hena/README.md
@@ -0,0 +1,7 @@
+|name|file|
+|:--|:--|
+|マスクしても同じ数|tyama_yokohamarb103.rb|
+|CoSVON|https://github.com/cielavenir/cosvon/|
+|カードゲームの役の判定|https://github.com/cielavenir/codeiq_solutions/blob/master/nabetani_takenori/tyama_codeiq476.cpp|
+|めぐるセル(簡易版)|tyama_hena25_yokohamarb.rb|
+|Rails on Tiles|tyama_hena5pre_yokohamarb.rb|
---
+++ hena/tyama_yokohamarb103.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+#http://nabetani.sakura.ne.jp/yokohamarb/103mask/
+#https://qiita.com/Nabetani/items/35e83eb39fed3c75d586
+
+def bits_to_num(bits)
+    bits.sum{|b|1<<b}
+end
+def output(r)
+    if r.size>15
+        puts [*r[0...13],'...',*r[-2..-1]]*','
+    else
+        puts r*','
+    end
+end
+while gets
+    msk=$_.to_i(16)
+    b=msk.to_s(2).reverse.each_char.with_index.select{|e,i|e=='1'}.map(&:last)
+    r={bits_to_num(b)=>1,bits_to_num(b[1..-1])=>1}
+    (1...1<<[b.size,4].min).each{|bit_searcher|
+        r[bits_to_num(b.select.with_index{|e,i|bit_searcher[i]>0})]=1
+    }
+    output(r.keys.sort)
+    STDOUT.flush
+end
