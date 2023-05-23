f6dd901a73f5abf0e7ba6a5c2f1417b268df9e86
cielavenir <cielartisan@gmail.com>
2023-05-23 14:02:30 +0900

    reworked on kanagawarb_evalex using refinement

--- hena/tyama_kanagawarb_evalex_eval.rb
+++ hena/tyama_kanagawarb_evalex_eval.rb
@@ -1,17 +1,26 @@
 #!/usr/bin/ruby
 #http://nabetani.sakura.ne.jp/kanagawa.rb/evalex/
 
-class Fixnum
-	alias_method :or_real,:|
-	alias_method :and_real,:&
-	alias_method :plus_real,:+
-	alias_method :mult_real,:*
-	def |(other) self.mult_real(other) end
-	def &(other) self.plus_real(other) end
-	def +(other) self.and_real(other) end
-	def *(other) self.or_real(other) end
+Fixnum=Integer if !defined?(Fixnum)
+module FixnumHack
+	refine Fixnum do
+		alias_method :or_real,:|
+		alias_method :and_real,:&
+		alias_method :plus_real,:+
+		alias_method :mult_real,:*
+		def |(other) self.mult_real(other) end
+		def &(other) self.plus_real(other) end
+		def +(other) self.and_real(other) end
+		def *(other) self.or_real(other) end
+	end
+end
+class Impl
+	using FixnumHack
+	def solve(s)
+		eval(s.tr('*+&|','|&+*'))
+	end
 end
 
 while gets
-	p eval($_.tr('*+&|','|&+*'))
+	p Impl.new.solve $_
 end
