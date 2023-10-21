4cf62f6519d97f8064d1537b86c35f7f70946cba
cielavenir <cielartisan@gmail.com>
2023-10-22 01:16:16 +0900

    optimize 2-3

--- topsic/pgbattle2023/2-3.rb
+++ topsic/pgbattle2023/2-3.rb
@@ -1,5 +1,5 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 N,K,*a=`dd`.split.map &:to_i
 M=998244353;I=[1,1];*F=I;*R=I
 2.upto(N){|i|I<<z=(M-M/i)*I[M%i]%M;F<<i*F[-1]%M;R<<z*R[-1]%M}
-p K>0?a.group_by.with_index{|e,i|i%K}.each_value.reduce(1){|s,v|v.group_by(&:to_i).each_value.reduce(s*F[v.size]){|s,_|s*R[_.size]}%M}:1
+p K>0?a.group_by.with_index{|_,i|i%K}.each_value.reduce(1){|s,v|v.tally(&:to_i).each_value.reduce(s*F[v.size]){|s,e|s*R[e]}%M}:1
