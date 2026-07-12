0376e3607fdac777ed06411892a6880a07e98313
cielavenir <cielartisan@gmail.com>
2026-07-13 02:16:21 +0900

    cheap pruning

--- hena/tyama_henap1_naive.c
+++ hena/tyama_henap1_naive.c
@@ -5,16 +5,23 @@
 #include <string.h>
 int solve(int b, int n){
 	int C[1999]; // put the memory as stack.
-	int m=0, i=1;
-	for(;;++i){
+	int m=0, i=b*b+b+1;
+	for(;;){
 		memset(C, 0, b*sizeof(int));
-		int x=i;
+		int x=i,redun=1;
 		for(;x>0;x/=b){
-			if(++C[x%b]>=3)break;
+			int c=++C[x%b];
+			if(c>=3){redun=3;break;}
+			if(c>=2)redun=2;
 		}
-		if(x>0){//break above
+		if(redun==3){
 			m++;
 			if(m>=n)return i;
+		}
+		if(redun==1){
+			i=i/b*b+b; // goto next n...n0(b) (set lowest digit 0)
+		}else{
+			i+=1;
 		}
 	}
 }
